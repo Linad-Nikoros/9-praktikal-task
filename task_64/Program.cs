00c4ea00931a0fbf96c5560156d6260113a76da6
Linad-Nikoros <linad.nikoros@gmail.com>
2022-11-25 14:05:53 +0700

    solved task64 and task66

---
+++ task_64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии
+Console.WriteLine("Введите N");
+int num1=Convert.ToInt32(Console.ReadLine());
+if (num1>=1)
+{
+Console.WriteLine("Введеное число должно быть меньше 1");
+}
+else
+{
+PrintNumber(num1);
+}
+
+void PrintNumber(int num1)
+{
+if (num1<=1)
+{
+Console.WriteLine($"{num1}");
+PrintNumber(num1+1);
+ num1++;
+}
+}
---
+++ task_66/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Введите M");
+int num1=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите N");
+int num2=Convert.ToInt32(Console.ReadLine());
+int sum=0;
+
+PrintNumber(num1,num2);
+
+Console.WriteLine($"{sum}");
+
+void PrintNumber(int num1,int num2)
+{
+if (num1<=num2)
+{
+ sum=sum+num1;
+num1++;
+PrintNumber(num1,num2);
+}
+}
+
+
